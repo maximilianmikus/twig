1f1ea06e8c64a0a00e5085830c8e34ec00a7688f
Maximilian Mikus <maximilianmikus@gmail.com>
2013-03-17 00:53:50 +0100

    added inline editing functionality

--- client/javascripts/client.js
+++ client/javascripts/client.js
@@ -1,39 +1,13 @@
 // Client-side JavaScript, bundled and sent to client.
 
-// Define Minimongo collections to match server/publish.js.
-Data = new Meteor.Collection("data");
-/*
-// ID of currently selected list
-Session.setDefault('user_id', null);
+// init session vars
 
-// Name of currently selected tag for filtering
-Session.setDefault('tag_filter', null);
+Session.set('editing', null);
 
-// When adding tag to a todo, ID of the todo
-Session.setDefault('editing_addtag', null);
-
-// When editing a list name, ID of the list
-Session.setDefault('editing_listname', null);
-
-// When editing todo text, ID of the todo
-Session.setDefault('editing_itemname', null);
-*/
-/*
-// Subscribe to 'lists' collection on startup.
-// Select a list once data has arrived.
-var listsHandle = Meteor.subscribe('lists', function () {
-  if (!Session.get('list_id')) {
-    var list = Lists.findOne({}, {sort: {name: 1}});
-    if (list)
-      Router.setList(list._id);
-  }
-});
-*/
 var dataHandle = null;
 
-console.log("user_id: "+Meteor.userId());
 
-// Always be subscribed to the todos for the selected list.
+// Always be subscribed to the data for the logged in user.
 Deps.autorun(function () {
   var user_id = Meteor.userId();
   if (user_id) {
@@ -44,54 +18,19 @@ Deps.autorun(function () {
 });
 
 
-////////// Helpers for in-place editing //////////
-/*
-// Returns an event map that handles the "escape" and "return" keys and
-// "blur" events on a text input (given by selector) and interprets them
-// as "ok" or "cancel".
-var okCancelEvents = function (selector, callbacks) {
-  var ok = callbacks.ok || function () {};
-  var cancel = callbacks.cancel || function () {};
-
-  var events = {};
-  events['keyup '+selector+', keydown '+selector+', focusout '+selector] =
-    function (evt) {
-      if (evt.type === "keydown" && evt.which === 27) {
-        // escape = cancel
-        cancel.call(this, evt);
-
-      } else if (evt.type === "keyup" && evt.which === 13 ||
-                 evt.type === "focusout") {
-        // blur/return/enter = ok/submit if non-empty
-        var value = String(evt.target.value || "");
-        if (value)
-          ok.call(this, value, evt);
-        else
-          cancel.call(this, evt);
-      }
-    };
-
-  return events;
-};
-
-var activateInput = function (input) {
-  input.focus();
-  input.select();
-};
-*/
-/////////// Saving Data Helper /////////////
+/////////// Saving & Updating Data Helpers /////////////
 
 
 var savedata = function(selector) {
-  var user_id = Meteor.userId();
-  var date = $("#date").val();
-  var docnr = $("#docnr").val();
-  var text = $("#text").val();
-  var earning = $("#earning").val();
-  var spending = $("#spending").val();
-  var vat = $("#vat").val();
-  var catnr = $("#catnr").val();
-  var cat = $("#cat").val();
+  var user_id = Meteor.userId(),
+      date = $(selector+" .dt__column__input--date").val(),
+      docnr = $(selector+" .dt__column__input--docnr").val(),
+      text = $(selector+" .dt__column__input--text").val(),
+      earning = $(selector+" .dt__column__input--earning").val(),
+      spending = $(selector+" .dt__column__input--spending").val(),
+      vat = $(selector+" .dt__column__input--vat").val(),
+      catnr = $(selector+" .dt__column__input--catnr").val(),
+      cat = $(selector+" .dt__column__input--cat").val();
   Data.insert({
                 user_id: user_id,
                 date: date,
@@ -105,6 +44,27 @@ var savedata = function(selector) {
   });
 }
 
+var updatedata = function(id, selector) {
+  var date = $(selector+" .dt__column__input--date").val(),
+      docnr = $(selector+" .dt__column__input--docnr").val(),
+      text = $(selector+" .dt__column__input--text").val(),
+      earning = $(selector+" .dt__column__input--earning").val(),
+      spending = $(selector+" .dt__column__input--spending").val(),
+      vat = $(selector+" .dt__column__input--vat").val(),
+      catnr = $(selector+" .dt__column__input--catnr").val(),
+      cat = $(selector+" .dt__column__input--cat").val();
+  Data.update(id,{$set: {
+                date: date,
+                docnr: docnr,
+                text: text,
+                earning: earning,
+                spending: spending,
+                vat: vat,
+                catnr: catnr,
+                cat: cat
+  }});
+}
+
 
 ////////// Todos //////////
 
@@ -113,16 +73,18 @@ Template.data.loading = function () {
 };
 
 
-Template.data.events({
+Template.dt_input.events({
   // Fires when submit button is clicked
-  'click #submit_new_data': function (event) { 
-    savedata();
+  'click #submit_new_data': function () {
+    var sel = "#dt_input"
+    savedata(sel);
   }
 });
 
+
 Template.data.data = function () {
-  // Determine which todos to display in main pane,
-  // selected based on list_id and tag_filter.
+  // Determine which data to display in main pane,
+  // selected based on user_id.
 
   var user_id = Meteor.userId();
   if (!user_id) {
@@ -133,11 +95,27 @@ Template.data.data = function () {
   return Data.find(sel, {sort: {timestamp: 1}});
 };
 
+Template.dt_row.editing = function () {
+  return Session.equals('editing', this._id);
+};
+
+Template.dt_row.id = function () {
+  return this._id;
+};
+
 Template.dt_row.events({
   'click .destroy': function () {
     Data.remove(this._id);
-    console.log(this._id);
-  }
+  },
+  'click .edit': function () {
+    Session.set('editing', this._id);
+  },
+  'click .save': function () {
+    var id = this._id;
+    var sel = "#"+id;
+    updatedata(id, sel);
+    Session.set('editing', null);
+  } 
 });
 
 ////////// Tracking selected list in URL //////////
