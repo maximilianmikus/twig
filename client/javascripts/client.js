e5702305018bf62add5dc9eec9a7dda743f409ac
Maximilian Mikus <maximilianmikus@gmail.com>
2013-04-04 19:51:58 +0200

    added balance

--- client/javascripts/client.js
+++ client/javascripts/client.js
@@ -222,18 +222,6 @@ Template.dt_row_cat_input.rendered = function() {
 
 ///////////////// Overview //////////////////////////
 
-/*
-Template.overview.data = function () {
-  //initcharts(Session.get('year'));
-  return Session.get('year');
-};
-
-Template.overview.init = function(year) {
-  //initcharts(year);
-  console.log("ding: "+year);
-};
-*/
-
 function initcharts(year) {
   // Build Chart Data
   var dataItems = Data.find({year: parseInt(year, 10)});
@@ -242,7 +230,7 @@ function initcharts(year) {
     labels : ["Jan","Feb","Mar","Apr","May","Jun","Jul", "Aug", "Sep", "Okt", "Nov", "Dez"],
     datasets : [
       {
-        fillColor : "rgba(154,205,50,0.5)",
+        fillColor : "rgba(154,205,50,0.7)",
         strokeColor : "rgba(154,205,50,1)",
         pointColor : "rgba(154,205,50,1)",
         pointStrokeColor : "#fff",
@@ -254,11 +242,21 @@ function initcharts(year) {
         pointColor : "rgba(255,69,0,1)",
         pointStrokeColor : "#fff",
         data : []
+      },
+      {
+        fillColor : "rgba(151,187,205,0.0)",
+        strokeColor : "rgba(151,187,205,0.8)",
+        pointColor : "rgba(151,187,205,1)",
+        pointStrokeColor : "#fff",
+        data : []
       }
     ]
   };
+  // init data arrays
   var spending = [0,0,0,0,0,0,0,0,0,0,0,0];
   var earning = [0,0,0,0,0,0,0,0,0,0,0,0];
+  var balance = [0,0,0,0,0,0,0,0,0,0,0,0];
+  // build earning & spending arrays
   $.each(arr, function() {
     var amount = parseFloat(this.amount.replace(",", "."));
     var month = parseMonth(this.date);
@@ -270,9 +268,15 @@ function initcharts(year) {
       spending[month] = spending_month+amount;
     }
   });
+  // substract spending from earning to get balance
 
-  data.datasets[0].data = earning;
+  for (var i=0; i < 11; i++) {
+    balance[i] = earning[i] - spending[i];
+  }
+
+  data.datasets[0].data = balance;
   data.datasets[1].data = spending;
+  data.datasets[2].data = earning;
 
   var ctx = document.getElementById("finance-chart").getContext("2d");
   var myNewChart = new Chart(ctx).Line(data);
