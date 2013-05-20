220044486460ac77cd51d286d9852634db99d4d4
Maximilian Mikus <maximilianmikus@gmail.com>
2013-05-20 13:31:59 +0200

    removed styling from user login

--- client/app.js
+++ client/app.js
@@ -36,35 +36,10 @@ Meteor.Router.add({
   '*': 'not_found'
 });
 
-/*
-Meteor.startup(function () {
-  var uid = Meteor.userId();
-  Config.insert({
-    user_id: uid,
-    categories: {
-      earning: [
-        {id: "0001", text: "Betriebseinnahmen"},
-        {id: "0002", text: "Betriebseinnahmen, keine Meldepflicht"},
-        {id: "0003", text: "Anlagenerträge"},
-        {id: "0004", text: "Übrige Betriebseinnahmen"}
-      ],
-      spending: [
-        {id: "1001", text: "Waren- Roh- und Hilfsstoffe"},
-        {id: "1002", text: "Beigestelltes Personal"},
-        {id: "1003", text: "Personalaufwand"},
-        {id: "1004", text: "AfA"},
-        {id: "1005", text: "Instandhaltung f. Gebäude"},
-        {id: "1006", text: "Reise-und Fahrtspäsen"},
-        {id: "1007", text: "KFZ-Kosten"},
-        {id: "1008", text: "Miete, Pacht, Leasing"},
-        {id: "1009", text: "Provisionen, Lizenzen"},
-        {id: "1010", text: "Werbeaufwändung"},
-        {id: "1011", text: "Buchwert abgegangener Anlagen"},
-        {id: "1012", text: "Zinsen &amp; ähnliches"},
-        {id: "1013", text: "Pflichtversicherung"},
-        {id: "1014", text: "Übrige Betriebsausgaben"}
-      ]
-    }
-  });
+Handlebars.registerHelper("loggedin", function() {
+  if (Meteor.userId()) {
+    return true;
+  } else {
+    return false;
+  }
 });
-*/
